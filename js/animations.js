97449182f781b95ec760ee441da00cc1284c90df
NIKOLAI KUCHUMOV <99647682+Nick763@users.noreply.github.com>
2024-04-04 00:23:11 +0400

    v2.7.2

--- js/animations.js
+++ js/animations.js
@@ -9,6 +9,18 @@ function updateScroll() {
 window.addEventListener('scroll', updateScroll);
 
 
+function hideScroll() {
+  if (window.scrollY > 4500) {
+    document.querySelector('.header__contaner').classList.add('header__contaner_hide');
+  } else {
+    document.querySelector('.header__contaner').classList.remove('header__contaner_hide');
+  }
+}
+
+window.addEventListener('scroll', hideScroll);
+
+
+
 function addSmoothScroll(anchor) {
   anchor.addEventListener('click', function (e) {
     e.preventDefault();
